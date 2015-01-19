220cae16b2adacd71d2baca97d814787eaa4513f
Shai Barack <shiber@gmail.com>
2015-01-19 12:02:05 -0800

    Tied rickroll gif to movie class

--- wear/src/main/java/com/shaibarack/rickrollwatchface/WatchFaceService.java
+++ wear/src/main/java/com/shaibarack/rickrollwatchface/WatchFaceService.java
@@ -1,7 +1,10 @@
 package com.shaibarack.rickrollwatchface;
 
+import android.graphics.Canvas;
 import android.graphics.Movie;
+import android.graphics.Rect;
 import android.support.wearable.watchface.CanvasWatchFaceService;
+import android.support.wearable.watchface.WatchFaceStyle;
 import android.view.SurfaceHolder;
 
 public class WatchFaceService extends CanvasWatchFaceService {
@@ -18,8 +21,18 @@ public class WatchFaceService extends CanvasWatchFaceService {
         @Override
         public void onCreate(SurfaceHolder holder) {
             super.onCreate(holder);
-
             mMovie = Movie.decodeStream(getResources().openRawResource(R.raw.rickroll));
+            setWatchFaceStyle(new WatchFaceStyle.Builder(WatchFaceService.this)
+                    .setShowSystemUiTime(true)
+                    .build());
+        }
+
+        @Override
+        public void onDraw(Canvas canvas, Rect bounds) {
+            // TODO check if ambient
+            mMovie.setTime((int) (System.currentTimeMillis() % mMovie.duration()));
+            mMovie.draw(canvas, 0, 0);
+            invalidate();
         }
     }
 }
